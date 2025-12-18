154691868c79380af3d0dae427bc29c1fa52d86d
Raniah Maritza <raniahmrz14@gmail.com>
2025-12-18 23:44:03 +0700

    revisi kembali modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -5,30 +5,30 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Load dataset preprocessing
-df = pd.read_csv('loan_approval_preprocessing.csv')
+# Load dataset
+df = pd.read_csv("loan_approval_preprocessing.csv")
 
-# Split data
-X = df.drop(columns='loan_approved')
-y = df['loan_approved']
+X = df.drop(columns="loan_approved")
+y = df["loan_approved"]
 
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Set experiment
 mlflow.set_experiment("CI Advance Training")
 
-# autolog
-mlflow.sklearn.autolog()
+with mlflow.start_run():
 
-model = LogisticRegression(max_iter=1000)
-model.fit(X_train, y_train)
+    mlflow.sklearn.autolog()
 
-y_pred = model.predict(X_test)
-acc = accuracy_score(y_test, y_pred)
-mlflow.log_metric("accuracy", acc)
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
 
-mlflow.sklearn.log_model(model, artifact_path="model")
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
 
-print(f"Accuracy: {acc}")
+    mlflow.log_metric("accuracy", acc)
+
+    mlflow.sklearn.log_model(model, artifact_path="model")
+
+    print(f"Accuracy: {acc}")
